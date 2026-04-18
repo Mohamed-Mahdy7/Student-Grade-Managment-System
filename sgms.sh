da2da737aeafcbcc2c79e579a9a81f63ebc12c56
Yamen <yamen.halim@gmail.com>
2026-04-18 17:26:44 +0200

    Student Transcript and Subject Statistics Reports Done

--- sgms.sh
+++ sgms.sh
@@ -911,7 +911,7 @@ view_grades_by_student(){
 
 # Grade Menu
 grade_menu(){
-    select opt in "Assign" "Update" "Delete" "ViewSubject" "ViewStudent" "Exit"
+    select opt in "Assign Grade to Student" "Update Grade" "Delete Grade" "View Grades by Subject" "View Grades by Student" "Exit"
     do
         case $REPLY in
             1)
@@ -942,6 +942,215 @@ grade_menu(){
     done
 }
 
+# Report Functions
+student_transcript(){
+    echo ================================
+    echo Choose which Student to view:
+    echo ================================
+    while true
+    do
+        read -p "Type the ID of the student you want to view: " student_id
+        if student_exist $student_id
+        then
+            break
+        fi
+    done
+    echo ================================
+    echo ====== Student Transcript ======
+    echo ================================
+    for file in ./sgms_data/grades/*.grd
+    do
+        line=$(grep "^${student_id}|" "$file")
+        if [[ -n "$line" ]]
+        then
+            std_id=$(echo "$line" | cut -d'|' -f1)
+            score=$(echo "$line" | cut -d'|' -f2)
+            letter=$(echo "$line" | cut -d'|' -f3)
+            
+            sub_code=$(basename "$file" .grd)
+            sub_name=$(sed -n '2p' "./sgms_data/subjects/${sub_code}.sub")
+
+            GPA=$(score_to_gpa "$score")
+
+            echo "$sub_code | $sub_name | $score | $letter | $GPA"
+        fi
+    done
+    CGPA=$(calculate_weighted_gpa "$std_id")
+    echo ================================
+    echo "Cumulative GPA: $CGPA"
+
+}
+
+subject_statistics(){
+    echo ================================
+    echo Choose which Subject to view:
+    echo ================================
+    while true
+    do
+        read -p "Type the ID of the Subject you want to view: " subject_id
+        if subject_exist $subject_id
+        then
+            break
+        fi
+    done
+    echo ================================
+    echo ====== Subject Statistics ======
+    echo ================================
+    total=0
+    count=0
+    avg=0
+    passC=0
+    pass=0
+    max=0
+    min=0
+    countAplus=0
+    countA=0
+    countAminus=0
+    countBplus=0
+    countB=0
+    countBminus=0
+    countCplus=0
+    countC=0
+    countCminus=0
+    countD=0
+    countF=0
+    for line in $(cat ./sgms_data/grades/${subject_id}.grd)
+    do
+        score=$(echo "$line" | cut -d'|' -f2)
+        letter=$(echo "$line" | cut -d'|' -f3)
+        
+        if [[ $count -eq 0 ]]
+        then
+            min=$score
+        fi
+        
+        max=$(echo "$max $score" |
+        awk '{
+            if ($2>$1)
+                print $2
+        }')
+        min=$(echo "$min $score" |
+        awk '{
+            if ($1>$2)
+                print $2
+        }')
+
+        total=$(echo "$total $score" |
+        awk '{
+            print $1+$2
+        }')
+        count=$((count+1))
+        
+        if [[$score>=50]]
+        then
+            passC=$((passC+1))
+        fi
+
+        if [[ "$letter" == "A+" ]]
+        then
+            countAplus=$((countAplus+1))
+        fi
+
+        if [[ "$letter" == "A" ]]
+        then
+            countA=$((countA+1))
+        fi
+
+        if [[ "$letter" == "A-" ]]
+        then
+            countAminus=$((countAminus+1))
+        fi
+
+        if [[ "$letter" == "B+" ]]
+        then
+            countBplus=$((countBplus+1))
+        fi
+
+        if [[ "$letter" == "B" ]]
+        then
+            countB=$((countB+1))
+        fi
+
+        if [[ "$letter" == "B-" ]]
+        then
+            countBminus=$((countBminus+1))
+        fi
+
+        if [[ "$letter" == "C+" ]]
+        then
+            countCplus=$((countCplus+1))
+        fi
+
+        if [[ "$letter" == "C" ]]
+        then
+            countC=$((countC+1))
+        fi
+
+        if [[ "$letter" == "C-" ]]
+        then
+            countCminus=$((countCminus+1))
+        fi
+
+        if [[ "$letter" == "D" ]]
+        then
+            countD=$((countD+1))
+        fi
+
+        if [[ "$letter" == "F" ]]
+        then
+            countF=$((countF+1))
+        fi
+    done
+    avg=$(echo "$total $count" |
+    awk '{
+        print $1/$2
+    }')
+    pass=$(echo "$passC $count" |
+    awk '{
+        print $1/$2
+    }')
+    
+    echo "Average Score: $avg"
+    echo "Highest Score: $max"
+    echo "Lowest Score: $min"
+    echo "Pass Rate: $pass"
+    echo "A+: $countAplus | A: $countA | A-: $countAminus"
+    echo "B+: $countBplus | B: $countB | B-: $countBminus"
+    echo "C+: $countCplus | C: $countC | C-: $countCminus"
+    echo "D: $countD"
+    echo "F: $countF"
+    echo ================================
+}
+
+# Reports Menu
+report_menu(){
+    select opt in "Student Transcript" "Subject Statistics" "Top" "Failing" "Matrix" "Exit"
+    do
+        case $REPLY in
+            1)
+            student_transcript
+            ;;
+            2)
+            subject_statistics
+            ;;
+            3)
+            ;;
+            4)
+            ;;
+            5)
+            ;;
+            6)
+            echo "exiting..."
+            return 0
+            ;;
+            *)
+            echo Invalid Selection!
+            echo Try Again
+            continue
+            ;;
+        esac
+    done
+}
 
 menu() {
     echo Main Menu
